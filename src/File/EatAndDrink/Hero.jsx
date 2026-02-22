f89e0ece333b74d83a07ca82b7fe9a6d2681943a
Nazmul Hasan <nazmulhasan9641@gmail.com>
2026-02-22 11:48:08 +0600

    img upadate

--- src/File/EatAndDrink/Hero.jsx
+++ src/File/EatAndDrink/Hero.jsx
@@ -5,7 +5,7 @@ import { Button } from '@heroui/button'
 function Hero() {
     return (
     <>
- <section className="bg-[url('eat-and-drink-hero-bg.png')] w-full h-screen overflow-hidden bg-cover bg-center ">
+ <section className="bg-[url('/eat-and-drink-hero-bg.png')] w-full h-screen overflow-hidden bg-cover bg-center ">
         <div className='w-fit md:w-[40%] ml-30 px-10 md:px-20 py-10 md:mt-30 justify-star bg-[#FBFCFF]/60 rounded-2xl justify-start'>
 
             <h1 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-black leading-tight'>Our top pick for the week!</h1>
